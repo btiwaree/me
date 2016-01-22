e2e7501d67b074024623a521fc79c10abf4dfbcd
Bishesh Tiwaree <b.tiwaree@jacobs-university.de>
2016-01-22 21:23:35 +0100

    adding all files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bishesh on 20.01.16.
+ */
+
+$(document).ready(function(){
+    $('.collapsible').collapsible({
+        accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
